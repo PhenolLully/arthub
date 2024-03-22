9a1227151e6fe260d532a06253317b66f2aac76d
Phenol Lully <lullyp95@gmail.com>
2024-03-21 21:12:41 -0400

    Added Account folder under components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Home from './components/Home/Home'; // Import the Home component
-import Profile from './components/Profile/Profile'; // Import the Profile component
+import Profile from './components/Pages/Profile'; // Import the Profile component
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [page, setPage] = useState('home');
+
+  const delayedSetPage = (newPage) => {
+    setTimeout(() => {
+      setPage(newPage);
+    }, 500); // Delay of 1 second (1000 milliseconds)
+  };
 
   const renderPage = () => {
     switch (page) {
@@ -27,11 +34,12 @@ function App() {
         <Container>
           <Navbar.Brand>ArtHub</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={() => setPage('home')}>Home</Nav.Link>
-            <Nav.Link onClick={() => setPage('profile')}>Profile</Nav.Link>
+            <Nav.Link onClick={() => delayedSetPage('home')}>Home</Nav.Link>
+            <Nav.Link onClick={() => delayedSetPage('profile')}>Profile</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      
       {renderPage()} {/* Render the current page based on the state */}
       
       <footer>
