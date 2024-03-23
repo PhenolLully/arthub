c101cc69e957cafdbf3711f84e06ee6d7b853836
unknown <dannysaravia100@gmail.com>
2024-03-23 18:45:12 -0400

    Fixed button/ disabled ripple

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
 export default function Post() {
   return (
-    <MDBBtn size='lg' floating style={{ backgroundColor: '#ac2bac' }} href='#'>
+    <MDBBtn size='lg' floating style={{ backgroundColor: '#ac2bac', boxShadow: 'none', borderRadius: '50%', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', disableRipple: true }} href='#'>
       <MDBIcon fab icon='instagram' />
     </MDBBtn>
   );
