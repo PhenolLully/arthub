f92e76731365357d570b38575bca7bbb995e1aed
unknown <dannysaravia100@gmail.com>
2024-03-23 20:25:48 -0400

    Fixed post button position

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
+import './Home.css'; // Assuming you have a Post.css file for styling
 
 export default function Post() {
   return (
-    <MDBBtn size='lg' floating style={{ backgroundColor: '#ac2bac', boxShadow: 'none', borderRadius: '50%', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', disableRipple: true }} href='#'>
-      <MDBIcon fab icon='instagram' />+
-    </MDBBtn>
+    <div className="post-btn-container">
+      <MDBBtn
+        size='lg'
+        floating
+        style={{ backgroundColor: '#ac2bac', boxShadow: 'none', borderRadius: '50%', width: '60px', height: '60px', display: 'flex', justifyContent: 'center', alignItems: 'center', disableRipple: true }}
+        href='#'
+      >
+        <MDBIcon fab icon='instagram' />
+        <span className="plus-sign">+</span>
+      </MDBBtn>
+    </div>
   );
 }
