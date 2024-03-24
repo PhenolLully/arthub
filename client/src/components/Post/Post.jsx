d9e83d0ef3490bcf9977924439e519e08e630c19
Phenol Lully <lullyp95@gmail.com>
2024-03-23 22:35:55 -0400

    Added more content to the create a post page

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,12 +1,31 @@
-import react from 'react';
+import React, { useState } from 'react';
 
-const Post = () => {
+export default function UploadImage() {
+    const [title, setTitle] = useState('');
+    const [images, setImages] = useState([]);
+    const [description, setDescription] = useState('');
+
+    function onImageChange(e) {
+        setImages([...e.target.files]);
+    }
+
     return (
-        <div>
-            <h1>Submit a Post!</h1>
-            {/* Add your post information here */}
-        </div>
+        <>
+            <h1 style = {{
+                color: 'white',
+            }}>Create a Post</h1>
+            <div style = {{}}>
+                <label htmlFor="title">Title:</label>
+                <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="image">Upload Image:</label>
+                <input type="file" id="image" multiple accept="image/*" onChange={onImageChange} />
+            </div>
+            <div>
+                <label htmlFor="description">Description:</label>
+                <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+            </div>
+        </>
     );
 }
-
-export default Post;
